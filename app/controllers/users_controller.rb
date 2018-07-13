186ef4b3987c062767e15ea5a9dd2bc995a124d2
Pedro5000 <pierre.de.vreyer@gmail.com>
2018-07-13 18:40:03 +0800

    Bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,7 @@ class UsersController < ApplicationController
     if logged_in? and !current_user.admin?
       flash[:danger] = "Only admin users can perform that action."
       redirect_to root_path
+    end
   end
 
 end
