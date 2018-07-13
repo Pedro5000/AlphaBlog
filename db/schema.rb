186962ae2668ff32d07b650356eeed4e44f304e1
Pedro5000 <pierre.de.vreyer@gmail.com>
2018-07-13 18:16:57 +0800

    Admin user creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708082331) do
+ActiveRecord::Schema.define(version: 20180713095242) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180708082331) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
